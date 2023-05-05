44c43f119b84b56b132ca69ab3cf1b73e3d80794
Björn Svensson <bjorn.a.svensson@est.tech>
2023-05-05 11:05:17 +0200

    Try-out flags

--- doc/examples/build.sh
+++ doc/examples/build.sh
@@ -1,8 +1,19 @@
-cc -O3 -fomit-frame-pointer -I../.. -o libco.o -c ../../libco.c
-c++ -O3 -fomit-frame-pointer -I../.. -c test_timing.cpp
-c++ -O3 -fomit-frame-pointer -o test_timing libco.o test_timing.o
-c++ -O3 -fomit-frame-pointer -I../.. -c test_args.cpp
-c++ -O3 -fomit-frame-pointer -o test_args libco.o test_args.o
-c++ -O3 -fomit-frame-pointer -std=c++11 -I../.. -c test_serialization.cpp
-c++ -O3 -fomit-frame-pointer -o test_serialization libco.o test_serialization.o
+#!/bin/sh
+FLAGS="${FLAGS:- -O3 -fomit-frame-pointer}"
+
+echo cc $FLAGS -I../.. -o libco.o -c ../../libco.c
+echo c++ $FLAGS -I../.. -c test_timing.cpp
+echo c++ $FLAGS -o test_timing libco.o test_timing.o
+echo c++ $FLAGS -I../.. -c test_args.cpp
+echo c++ $FLAGS -o test_args libco.o test_args.o
+echo c++ $FLAGS -std=c++11 -I../.. -c test_serialization.cpp
+echo c++ $FLAGS -o test_serialization libco.o test_serialization.o
+
+cc $FLAGS -I../.. -o libco.o -c ../../libco.c
+c++ $FLAGS -I../.. -c test_timing.cpp
+c++ $FLAGS -o test_timing libco.o test_timing.o
+c++ $FLAGS -I../.. -c test_args.cpp
+c++ $FLAGS -o test_args libco.o test_args.o
+c++ $FLAGS -std=c++11 -I../.. -c test_serialization.cpp
+c++ $FLAGS -o test_serialization libco.o test_serialization.o
 rm -f *.o
